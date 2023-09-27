3b2adc0627675b745a47ba7a0710954b2e0aa30c
Gustavo Henrique <gassuncaopaiva@gmail.com>
2023-09-27 16:02:11 -0300

    theme permanency

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector(".profile img")
-
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    localStorage.setItem("theme", "light")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+    localStorage.setItem("theme", "dark")
+  }
+}
+function prof() {
+  const html = document.documentElement
+  const img = document.querySelector(".profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
